d27bb018679802c9b27a17b432de94f1960ef0e4
Alexnadr Larin <allarin03@mail.ru>
2025-02-09 16:10:13 +0300

    Add cors and more logging

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -1,25 +1,51 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/rs/cors"
 
 	"backend/router"
 	"backend/internal/database"
 )
 
 func main() {
-	repo, err := database.NewRepository()
-    if err != nil {
-        log.Fatalf("Ошибка подключения к Postgres: %v", err)
-    }
-    defer repo.Close()
+	repo, err := database.NewRepository(10, 2*time.Second)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к базе данных: %v", err)
+	}
+	defer repo.Close()
 
 	r := router.SetupRouter(repo)
 
-	fmt.Println("Сервер запущен")
+	allowedOrigins := os.Getenv("CORS_ALLOWED_ORIGINS")
+	if allowedOrigins == "" {
+		log.Fatal("CORS_ALLOWED_ORIGINS не установлена")
+	}
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("BACKEND_PORT"), r))
+	originsList := strings.Split(allowedOrigins, ",")
+	for i, origin := range originsList {
+		originsList[i] = strings.TrimSpace(origin)
+	}
+
+	corsOptions := cors.Options{
+		AllowedOrigins:     originsList, 
+		AllowedMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials:   true,
+	}
+
+	corsHandler := cors.New(corsOptions).Handler(r)
+
+	backendPort := os.Getenv("BACKEND_PORT")
+	if backendPort == "" {
+		log.Fatal("BACKEND_PORT не установлена")
+	}
+
+	log.Println("Сервер запущен")
+	log.Fatal(http.ListenAndServe(":"+backendPort, corsHandler))
 }
