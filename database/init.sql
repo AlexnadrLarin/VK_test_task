93065e2a10370cf7b02e7b94fc8ce2c5b221c819
Alexnadr Larin <allarin03@mail.ru>
2025-02-09 16:10:49 +0300

    Change table types in db

--- database/init.sql
+++ database/init.sql
@@ -2,5 +2,5 @@ CREATE TABLE ping_results (
     id SERIAL PRIMARY KEY,
     ip VARCHAR(45) NOT NULL UNIQUE,    
     ping_time VARCHAR(50) NOT NULL,   
-    date TIMESTAMP NOT NULL            
+    date VARCHAR(50) NOT NULL            
 );
